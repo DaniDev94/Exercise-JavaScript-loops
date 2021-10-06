fe98575559debf282d5dc9c0f32625f3da871ffd
Daniel López Plaza <daniel.lp1094@gmail.com>
2021-10-06 23:51:54 +0200

    FEAT: iteration#5

--- iteration-5/iteration#5.js
+++ iteration-5/iteration#5.js
@@ -7,6 +7,32 @@ const placesToTravel = [
     {id: 59, name: 'Madagascar'},
 ];
 
+
+let newPlaces = [];
 for(let places of placesToTravel){
-    
-}
+   if((places.id !== 11) && (places.id !== 40)){
+    newPlaces.push(places)
+   };
+};
+console.log(newPlaces);
+
+
+
+
+console.log('Prueba 2________________________________________________________')
+
+
+
+let deletPlaces = [11, 40];
+for (let i = 0; i < placesToTravel.length; i++) {
+    let element = placesToTravel[i];
+  if(deletPlaces.includes(element.id)){
+    placesToTravel.splice(i,1);
+  };
+};
+console.log(placesToTravel)
+
+
+
+
+
